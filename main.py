0388ef3205ffddb86d3428036d411c10d7b423e9
Alexander Pechmann <alexander.pechmann@hm.edu>
2025-10-18 13:04:55 +0200

    Test if new ssh config works

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def say_hi() -> None:
     print("Hello World!")
 
 
+def test_if_ssh_works() -> None:
+    print("This is a test")
+
+
 if __name__ == '__main__':
     say_hi()
+    test_if_ssh_works()
